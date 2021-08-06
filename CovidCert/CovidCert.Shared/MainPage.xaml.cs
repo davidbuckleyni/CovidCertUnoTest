ea96896e20aac86e123d1db5e4ab80b24944108b
davidbuckleyni <davidbuckleyweb@gmail.com>
2021-08-06 20:25:31 +0100

    Add project files.

---
+++ CovidCert/CovidCert.Shared/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CovidCert
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            btn1.Foreground = new SolidColorBrush(Windows.UI.Colors.Blue);
+            text1.Text = "Click event occurs on Hover.";
+            text2.Text = "";
+            text3.Text = "";
+            
+        }
+
+    }
+}
---
+++ CovidCert/CovidCert.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace CovidCert.Skia.Gtk
+{
+}
